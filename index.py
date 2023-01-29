d6f8029a5bc9f742a41fcd0d663ebe4901c2e37d
Luis Lira <luislira1029@gmail.com>
2023-01-29 12:43:47 -0600

    initial commit

---
+++ index.py
@@ -0,0 +1,12 @@
+# This is a comment
+"""
+This is a comment
+with
+multiple lines
+"""
+print("Hello world")
+
+# Consultar el tipo de dato
+print(type("Hey, this is a string"))
+print(type(12))
+print(type(1.6))
